9e7d4aea071c4653c1174f0500991c5fcd2f51d2
Internship <internship@Joshuas-MacBook-Pro.local>
2021-12-23 14:09:48 -0500

    added dataset

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import random
+import numpy as np
 print("hello world")
+nums = [] 
+mu = 100
+sigma = 50
+    
+for i in range(100): 
+    temp = random.gauss(mu, sigma)
+    nums.append(temp) 
+
+a_file = open("test.txt", "w")
+np.savetxt(a_file, nums)
+a_file.close()
